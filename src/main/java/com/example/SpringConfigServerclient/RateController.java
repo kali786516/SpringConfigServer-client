cbc2466825529582a9103778903021d59894392b
kalit_000 <kali.tummala@gmail.com>
2019-06-24 16:07:18 -0400

    all Spring Projects

--- src/main/java/com/example/SpringConfigServerclient/RateController.java
+++ src/main/java/com/example/SpringConfigServerclient/RateController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RefreshScope
 @Controller
@@ -23,7 +24,7 @@ public class RateController {
     @Value("${connstring}")
     String connstring;
 
-    @RequestMapping("/rate")
+    @RequestMapping(value = "/rate",method = RequestMethod.GET)
     public String getRate(Model m) {
 
         m.addAttribute("rateamount", rate);
