0ecda2c7abba8a65affc6b7241c12a626d302037
kalit_000 <kali.tummala@gmail.com>
2019-06-24 14:37:36 -0400

    Initial commit

---
+++ src/main/java/com/example/SpringConfigServerclient/SpringConfigServerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringConfigServerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerClientApplication.class, args);
+	}
+
+}
